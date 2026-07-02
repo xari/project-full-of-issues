d0d4dd67168377954ec3fd78772dc0b04cfb8bb4
Harry Anderson <harry.anderson@sonarsource.com>
2026-07-02 17:54:30 +0200

    Create test-small-file.js

---
+++ test-small-file.js
@@ -0,0 +1,5 @@
+// problematic-sonar.js
+
+let password = '123456'; // Hardcoded password: Potential security issue
+
+let unusedVariable = 'I\'m not used'; // Unused variable: Code smell for dead code
